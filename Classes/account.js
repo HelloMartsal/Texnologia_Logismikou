64b8d86df69aa1a8668342c0cd4c4b5741c49864
Marios Tsaltakis <mariostsaltakis@gmail.com>
2024-05-18 20:01:27 +0300

    chore: Convert isLogged method to use async/await for improved readability and error handling

--- Classes/account.js
+++ Classes/account.js
@@ -17,25 +17,25 @@ export default class Account {
       }
   }
 
-  isLogged(req, res, next) {
+  async isLogged(req, res, next) {
     const userId = req.session.userId; // Get the user ID from the session
-  
-    // Query the database to check if the user ID belongs to an admin
-    this.db.query('SELECT username FROM log WHERE username = ?', [userId], (err, results) => {
-      if (err) {
+
+    try {
+        // Query the database to check if the user ID belongs to an admin
+        const [results] = await this.db.query('SELECT username FROM log WHERE username = ?', [userId]);
+        console.log(results);
+        
+
+        if (results.username) {
+            // User is an admin based on the query results
+            return next();
+        } else {
+            res.status(401).send('Unauthorized. Insufficient permissions.');
+        }
+    } catch (err) {
         console.error('Error fetching  users IDs:', err);
         res.status(500).send('Error occurred.');
-        return;
-      }
-  
-
-      if (results.length > 0) {
-        // User is an admin based on the query results
-        //res.send(`Admin ID: ${userId}`);
-        return next();
-      } else {
-        res.status(401).send('Unauthorized. Insufficient permissions.');
-      }
-    });
+    }
   }
+  
 }
