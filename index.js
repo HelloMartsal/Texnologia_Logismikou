7265d90a6908265f99dcf67fb033bb5d36b59c2e
GregNetworking <96661289+GregNetworking@users.noreply.github.com>
2024-05-26 18:27:30 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ import { fileURLToPath } from 'url';
 import session from "express-session";
 import bodyParser from'body-parser';
 import Account from "./Account.js";
+import specialty from "./specialty.js";
+import Service from "./service.js";
+import review from "./review.js";
+import reservation from "./reservation.js";
+import notification from "./notification.js";
+import Offer from "./offer.js";
+import Payment from "./payment.js";
+
 
 
 
@@ -15,6 +23,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -211,7 +220,7 @@ app.post('/',  (req, res) => {
       
      
         //res.redirect('/admin/:id'); 
-    } else if (message === 'Data transferred from citizen successfully.') {
+    } else if (message === 'Data transferred from user successfully.') {
       db.query(getUserIdSql, [username, passwd], (getUserIdErr, getUserIdResults) => {
         if (getUserIdErr) {
           console.error('Error getting user ID:', getUserIdErr);
@@ -236,7 +245,7 @@ app.post('/',  (req, res) => {
       });
 
         //res.redirect('/citizen');
-    } else if (message === 'Data transferred from rescuer successfully.') {
+    } else if (message === 'Data transferred from tech successfully.') {
       db.query(getUserIdSql, [username, passwd], (getUserIdErr, getUserIdResults) => {
         if (getUserIdErr) {
           console.error('Error getting user ID:', getUserIdErr);
@@ -273,16 +282,43 @@ app.post('/',  (req, res) => {
   });
   
 
+  app.get("/signup", (req, res) => {
+    res.sendFile(__dirname + '/signup.html');
+  });
+  
+  app.post('/signup', (req, res) => {
+    const { username, password, phone, name, surname, address, email } = req.body;
+    const date = new Date().toISOString().split('T')[0];
+    const addAccountSql = "CALL addAccount(?,?,?,?,?,?,?,?)";
+  
+    db.query(addAccountSql, [username, password, phone, name, surname, address, email, date], (err, results) => {
+      if (err) {
+        console.error('Error during signup query:', err);
+        res.status(500).send('Error occurred during signup');
+        return;
+      }
+  
+      const message = results[0][0].message;
+  
+      if (message === 'Account submitted successfully') {
+        res.redirect(`/`);
+      } else {
+        res.send(message);
+      }
+    });
+  });
 
 
   app.get('/user', isAuthenticated, isUser, isLogged, (req, res) => {
-    const query = 'SELECT username, password, name, surname, address,phone_number, email, date FROM account';
-   // res.send('Welcome user!');
-   db.query(query, (getUserIdErr, getUserIdResults) => {
-    const accounts = results.map(row => new Account(row.username, row.password, row.name, row.surname, row.address,row.phone_umber, row.email, row.date));
+  //   const query = 'SELECT username, password, name, surname, address,phone_number, email, date FROM account';
+  //  // res.send('Welcome user!');
+  //  db.query(query, (getUserIdErr, getUserIdResults) => {
+  //   const accounts = results.map(row => new Account(row.username, row.password, row.name, row.surname, row.address,row.phone_umber, row.email, row.date));
 
-    console.log(accounts);
-  });
+  //   console.log(accounts);
+  // });
+
+  res.sendFile(__dirname + '/menu.html');
   });
 
   app.get('/tech', isAuthenticated, isTech, isLogged, (req, res) => {
@@ -308,6 +344,114 @@ app.post('/',  (req, res) => {
   });
 
 
+  app.get("/user/account", (req, res) => {
+    res.sendFile(__dirname + '/account.html');
+    
+});
+
+
+
+app.get("/user/accountinfo", (req, res) => {
+ const userId=req.session.userId
+ const info='CALL getUserInfo(?)'
+
+ db.query(info, [userId], (getUserIdErr, getUserIdResults) => {
+
+  res.json(getUserIdResults);
+  console.log(getUserIdResults)
+  
+});
+
+  
+});
+
+
+
+app.post('/user/updateinfo', (req, res) => {
+  const userId = req.session.userId;
+  const { field, value } = req.body;
+  const query = `UPDATE account SET ${field} = ? WHERE username = ?`;
+
+  db.query(query, [value, userId], (err, results) => {
+      if (err) {
+          console.error('Error updating user info:', err);
+          res.status(500).send('Error occurred while updating user info');
+          return;
+      }
+      res.json({ field, value });
+      //res.send('Load succeded');
+  });
+});
+
+app.get('/find_tech', isAuthenticated, isUser, isLogged, (req, res) => {
+  res.sendFile(__dirname + '/find_tech.html');
+});
+//find a middleware to stop access
+app.get('/api/techs', isAuthenticated, isUser, isLogged, async (req, res) => {
+  try{
+    const specialities = await Specialty.getAllSpecialties(db);
+    const services = await Service.getAllServices(db);
+    res.json({ data: { specialities, services } });
+  } catch (err) {
+    console.error('Error fetching specialities and services:', err);
+    res.status(500).send('Error occurred');
+    }
+});
+//we cash the techs to avoid multiple queries
+let cachedTechs = null;
+app.post('/api/techs', isAuthenticated, isUser, isLogged, async (req, res) => {
+  if(req.body.called){
+    //console.log("called");
+    //here we do the filtering and return the techs
+
+  }else {
+    try {
+      const techs = await Tech.getAllTechs(db);
+      cachedTechs = techs;
+      res.json({ data: techs });
+    } catch (err) {
+      console.error('Error fetching techs:', err);
+      res.status(500).send('Error occurred');
+    }
+  }
+});
+
+app.get('/booking', isAuthenticated, isUser, isLogged, async (req, res) => {
+  res.sendFile(__dirname + '/booking.html');
+});
+
+app.get('/api/booking', isAuthenticated, isUser, isLogged, async (req, res) => {
+  try {
+    const bookings = await Reservations.getBookingHistory(db,user.username);
+    res.json({ data: bookings });
+  } catch (err) {
+    console.error("Error fetching booking history:", err);
+    res.status(500).send("Error occurred");
+  }
+});
+
+app.get('/calendar', isAuthenticated, isTech, isLogged, async (req, res) => {
+  res.sendFile(__dirname + '/calendar.html');
+});
+
+app.get('/api/calendar', isAuthenticated, isTech, isLogged, async (req, res) => {
+  try {
+    const availabity =await tech.getAvailabity(db);
+    res.json({ data: availabity });
+  } catch (err) {
+    console.error("Error fetching calendar:", err);
+    res.status(500).send("Error occurred");
+  }
+});
+app.post('/api/calendar', isAuthenticated, isTech, isLogged, async (req, res) => {
+  const events = req.body;
+  try {
+    await tech.setAvailability(db, events);
+  } catch (err) {
+    console.error("Error setting availability:", err);
+    res.status(500).send("Error occurred");
+  }
+});
 
 app.listen(8800, () => {
     console.log("Connected to backend.");
